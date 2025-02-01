4de179af57b265b55a629d2d8378d594b769944d
ChristiannYa <christian.ylopez02@gmail.com>
2025-02-01 14:52:07 -0600

    swapped fax order

--- client/src/constants/faqs.js
+++ client/src/constants/faqs.js
@@ -18,21 +18,21 @@ export const faqs = [
   },
   {
     id: 3,
+    q: 'What is the estimated timeframe for editing the images after the photo shoot?',
+    a: [
+      "After the photo shoot, we'll carefully select your favorite shots, and I'll start editing them. This process typically takes about a week or two.",
+      "If you'd like to include more photos, there's an option to do so for a small additional fee.",
+      "Once everything is ready, you'll receive access to your personalized online gallery with all the edited photos.",
+    ],
+  },
+  {
+    id: 4,
     q: 'How soon after the photoshoot are the proofs ready for viewing?',
     a: [
       'Portaiture Sessions:',
       'The final proofing takes 1 week to be ready.',
       'A sneak preview will be published on our website and social media within 3 days after the shooting.',
       'Event final proofing turn around time: 2 weeks.',
-    ],
-  },
-  {
-    id: 4,
-    q: 'What is the estimated timeframe for editing the images after the photo shoot?',
-    a: [
-      "After the photo shoot, we'll carefully select your favorite shots, and I'll start editing them. This process typically takes about a week or two.",
-      "If you'd like to include more photos, there's an option to do so for a small additional fee.",
-      "Once everything is ready, you'll receive access to your personalized online gallery with all the edited photos.",
     ],
   },
 ];
