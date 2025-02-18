62d089f677f429f41bbfdd5a3bd454a7d2bd3651
ChristiannYa <christian.ylopez02@gmail.com>
2025-02-17 19:10:51 -0600

    landing text change

--- client/src/sections/home/SlideshowText.jsx
+++ client/src/sections/home/SlideshowText.jsx
@@ -1,7 +1,7 @@
 const SlideshowText = () => {
   return (
     <div className="bg-none wh-full fixed">
-      <div className="text-center uppercase absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full mb-56 xl:mb-48">
+      <div className="text-center uppercase absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full mb-48">
         <h1 className="font-fNewsreader text-2xl lg:text-3xl ttsm tracking-widest">
           Tania Gutti
         </h1>
