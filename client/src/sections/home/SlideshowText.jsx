8ed7e2aa8099eee57b256479838edbb5aa3c51eb
ChristiannYa <christian.ylopez02@gmail.com>
2025-06-12 20:41:50 -0500

    updated landing font

--- client/src/sections/home/SlideshowText.jsx
+++ client/src/sections/home/SlideshowText.jsx
@@ -2,10 +2,10 @@ const SlideshowText = () => {
   return (
     <div className="bg-none wh-full fixed">
       <div className="text-center uppercase absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full mb-48">
-        <h1 className="font-fNewsreader text-2xl lg:text-3xl ttsm tracking-widest">
+        <h1 className="font-fCinzel text-2xl lg:text-3xl ttsm tracking-widest">
           Tania Gutti
         </h1>
-        <h1 className="font-fNewsreader text-xs sm:text-xl tracking-widest text-white">
+        <h1 className="font-fCinzel text-xs sm:text-xl tracking-widest text-white">
           Photography | Films | Content Creation
         </h1>
       </div>
