6476be22b423570cdec111530b8a252fe94663d2
ChristiannYa <christian.ylopez02@gmail.com>
2026-09-18 15:00:25 -0500

    info update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,42 +2,60 @@ import icons from "../assets/icons";
 // import Translate from '../components/Translate';
 
 const Footer = () => {
-  return (
-    <footer className="shadow-bs-2 colflex-center w-full h-fit py-6">
-      <div className="rowflex-center gap-x-16 gap-y-3 max-sm:flex-col">
-        <aside className="colflex-center gap-y-2 gap-x-1 max-sm:flex-row max-sm:gap-x-3">
-          <a
-            href="https://www.instagram.com/taniaguttiphotography"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={icons.instagram} alt="" width={24} height={24} />
-          </a>
-          <img src={icons.txFlag} alt="" width={30} height={30} />
-          <a href="tel:+17138101895" className="sm:hidden">
-            <img src={icons.phone} alt="" width={24} height={24} />
-          </a>
-        </aside>
-        <div className="colflex-center gap-1">
-          <p className="footer-text capitalize">
-            ©2026 haute bokeh by tania gutti
-          </p>
-          <p className="footer-text capitalize">all rights reserved</p>
-        </div>
-        <div className="colflex-center opacity-50">
-          <p className="footer-text">Designed by</p>
-          <a
-            href="https://christianweb.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p className="footer-text capitalize">christianwebdev</p>
-          </a>
-        </div>
-        {/* <Translate /> */}
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="shadow-bs-2 colflex-center w-full h-fit py-6">
+            <div className="rowflex-center gap-x-16 gap-y-3 max-sm:flex-col">
+                <aside className="colflex-center gap-y-2 gap-x-1 max-sm:flex-row max-sm:gap-x-3">
+                    <a
+                        href="https://www.instagram.com/taniaguttiphotography"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img
+                            src={icons.instagram}
+                            alt=""
+                            width={24}
+                            height={24}
+                        />
+                    </a>
+                    <img
+                        src={icons.txFlag}
+                        alt=""
+                        width={30}
+                        height={30}
+                    />
+                    <a
+                        href="tel:+17138101895"
+                        className="sm:hidden"
+                    >
+                        <img
+                            src={icons.phone}
+                            alt=""
+                            width={24}
+                            height={24}
+                        />
+                    </a>
+                </aside>
+                <div className="colflex-center gap-1">
+                    <p className="footer-text capitalize">
+                        ©2026 haute bokeh llc
+                    </p>
+                    <p className="footer-text capitalize">all rights reserved</p>
+                </div>
+                <div className="colflex-center opacity-50">
+                    <p className="footer-text">Designed by</p>
+                    <a
+                        href="https://christianweb.dev"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <p className="footer-text capitalize">christianwebdev</p>
+                    </a>
+                </div>
+                {/* <Translate /> */}
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
